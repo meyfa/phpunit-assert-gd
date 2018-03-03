fd9ea5c3e70de4d11cf318fe25979d78abf2a55e
peter279k <peter279k@gmail.com>
2018-03-04 04:48:40 +0800

    fix the 5.3 trait test

--- tests/GDAssertTraitTest53.php
+++ tests/GDAssertTraitTest53.php
@@ -1,8 +1,8 @@
-<?php
+ <?php
 
 use PHPUnit\Framework\TestCase;
 
-use AssertGD\GDAssertTrait;
+use AssertGD\GDSimilarityConstraint;
 
 /**
  * @SuppressWarnings(PHPMD)
@@ -12,39 +12,39 @@ class GDAssertTraitTest extends TestCase
     public function testSamePath()
     {
         // should compare successfully
-        $this->assertSimilarGD('./tests/images/stripes-bw-10x10.png',
+        $this->assertThat('./tests/images/stripes-bw-10x10.png',
             new GDSimilarityConstraint('./tests/images/stripes-bw-10x10.png'));
     }
 
     public function testDifferentDimensions()
     {
         // should compare unsuccessfully
-        $this->assertNotSimilarGD('./tests/images/stripes-bw-10x10.png',
-            new GDSimilarityConstraint('./tests/images/stripes-bw-20x20.png'));
+        $this->assertThat('./tests/images/stripes-bw-10x10.png',
+            $this->logicalNot($this->equalTo(new GDSimilarityConstraint('./tests/images/stripes-bw-20x20.png'))));
     }
 
     public function testDifferentImages()
     {
         // should compare unsuccessfully
-        $this->assertNotSimilarGD('./tests/images/stripes-bw-10x10.png',
-            new GDSimilarityConstraint('./tests/images/stripes-bw-10x10-alt.png'));
+        $this->assertThat('./tests/images/stripes-bw-10x10.png',
+            $this->logicalNot($this->equalTo(new GDSimilarityConstraint('./tests/images/stripes-bw-10x10-alt.png'))));
     }
 
     public function testDifferentImagesThreshold1()
     {
         // should compare successfully
-        $this->assertSimilarGD('./tests/images/stripes-bw-10x10.png',
+        $this->assertThat('./tests/images/stripes-bw-10x10.png',
             new GDSimilarityConstraint('./tests/images/stripes-bw-10x10-alt.png', '', 1));
     }
 
     public function testJpeg()
     {
         // should compare unsuccessfully with threshold = 0.01
-        $this->assertNotSimilarGD('./tests/images/jpeg.jpg',
-            new GDSimilarityConstraint('./tests/images/jpeg-alt.jpg', '', 0.01));
+        $this->assertThat('./tests/images/jpeg.jpg',
+        $this->logicalNot($this->equalTo(new GDSimilarityConstraint('./tests/images/jpeg-alt.jpg', '', 0.01))));
 
         // should compare successfully with threshold = 0.1
-        $this->assertSimilarGD('./tests/images/jpeg.jpg',
+        $this->assertThat('./tests/images/jpeg.jpg',
             new GDSimilarityConstraint('./tests/images/jpeg-alt.jpg', '', 0.1));
     }
 }
